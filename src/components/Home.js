4d24b8a0f96d0ade8676232b70fe21d701d0f89d
James Burns <jrburns96@aol.com>
2021-11-23 16:57:26 -0500

    commit tree e0e98a71ff33bda710c81fa977a362aab5aeda52

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return(
+ <div id= "home">
+    <HomeChild/>
+  </div>)
 }
 
+const HomeChild = () => {
+  // include name and city variables (need to import)
+return (  <h1 style={{color: 'firebrick'}}>{name} is a Web Developer from {city}</h1>)
+
+}
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,22 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return(
+    <nav>
+    <Child1 />
+    <Child2 />
+    </nav>
+  ) 
+}
+const Child1 = () => {
+  return (
+    <a href='#home'>Home</a>
+  )
+}
+const Child2 = () => {
+  return (
+  <a href='#about'>About</a>
+  )
 }
 
 export default NavBar;
